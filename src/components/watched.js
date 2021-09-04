a283657b2f91a818d01d93e33ca3c37f394b33fb
Abhishek Chandrasekar <79383816+Abhishekcs96@users.noreply.github.com>
2021-09-04 11:40:10 +0530

    Update watched.js

--- src/components/watched.js
+++ src/components/watched.js
@@ -5,20 +5,7 @@ import Moviecard from './Moviecard';
 
 const Watched = () => {
     const {watched} = useContext(Globalcontext);
-    return (
-        <div className="movie-page">
-        <div className="container">
-            <div className="header">
-               <h1 className ='heading'>Watched Movies!</h1> 
-                   
-               {watched.length > 0 ? (  <div className="movie-grid">
-                {watched.map((Movie)=>(<Moviecard Movie ={Movie} type= "watched"/>))}
-            </div>) : (<h2 className='No-movies'> No movies! </h2>)} 
-            </div>
-            
-        </div>
-    </div>
-    )
+    return 
 }
 
 export default Watched;
